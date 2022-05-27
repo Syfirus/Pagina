76919cbcc981c01a4af60811eea6788ce0b3ea64
Syfirus <105953766+Syfirus@users.noreply.github.com>
2022-05-27 03:58:38 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function login(){
+var user, pass;
+
+user = document.getElementById("usuario").value;
+pass = document.getElementById("contraseña").value;
+
+if(user == "Syfirus" && pass == "Kingsman22" || user == "" && pass == "" ){
+
+
+ window.location="index.html"
+ location.href="index.html"
+
+}
+}
