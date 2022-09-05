4b86659402886652f0626f67aed9c75ffdc1805c
Syfirus <105953766+Syfirus@users.noreply.github.com>
2022-09-05 12:06:05 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function login(){
+var user, pass;
+
+user = document.getElementById("usuario").value;
+pass = document.getElementById("contraseña").value;
+
+if(user == "Syfirus" && pass == "Kingsman22" || user == "Invitado" && pass == "0101" ){
+
+
+ window.location="pagina/index.html"
+ location.href="pagina/index.html"
+
+}
+}
